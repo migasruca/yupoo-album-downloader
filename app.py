00c687664d155ea5cbe893abba93bf010dca6c9c
migasruca <168606958+migasruca@users.noreply.github.com>
2026-05-11 00:20:25 +0100

    Add function to download images from Yupoo

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+import requests
 
+def download_yupoo_img(url):
+    headers = {
+        "Referer": "https://yupoo.com/", # Essencial para o Yupoo permitir o acesso
+        "User-Agent": "Mozilla/5.0..."
+    }
+    response = requests.get(url, headers=headers)
+    return response.content
